8b49df0317bb8e9d78cc17b574aade9e76811406
Nikola Majcen <nikola.majcen@gmail.com>
2016-06-27 11:12:56 +0200

    Added basic layout

---
+++ SCVZ Canteen/Views/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  SCVZ Canteen
+//
+//  Created by Nikola Majcen on 27/06/16.
+//  Copyright © 2016 Nikola Majcen. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
