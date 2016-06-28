20630e226063ff254bb09619777ea3a2451ba431
Nikola Majcen <nikola.majcen@gmail.com>
2016-06-28 22:52:40 +0200

    Finalized main UI

--- SCVZ Canteen/Views/MenuViewController.swift
+++ SCVZ Canteen/Views/MenuViewController.swift
@@ -13,6 +13,11 @@ class MenuViewController: UIViewController {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var menuImage: UIImageView!
     
+    @IBOutlet weak var previousDayButton: UIButton!
+    @IBOutlet weak var nextDayButton: UIButton!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
     @IBOutlet weak var lunchButton: UIButton!
     @IBOutlet weak var dinnerButton: UIButton!
     
@@ -25,18 +30,78 @@ class MenuViewController: UIViewController {
         
         self.menuTable.delegate = self
         self.menuTable.dataSource = self
-                
-        self.view.backgroundColor = UIColor.flatSandColorDark()
-        self.headerView.backgroundColor = UIColor.flatSandColorDark()
         
-        self.menuTable.backgroundColor = UIColor.flatCoffeeColor()
-        self.lunchButton.backgroundColor = UIColor.flatCoffeeColor()
-        self.dinnerButton.backgroundColor = UIColor.flatCoffeeColorDark()
+        self.initializeUI()
+        self.initializeControls()
         
         // Example data
+        self.dateLabel.text = "28.06.2016."
+        
         self.menu.lunch = []
         self.createExampleMenu()
         self.menuTable.reloadData()
+    }
+    
+    @IBAction func showLunchMenu(sender: UIButton) {
+        changeMenuImage(sender)
+        changeButtonFocus(sender)
+    }
+    
+    @IBAction func showDinnerMenu(sender: UIButton) {
+        changeMenuImage(sender)
+        changeButtonFocus(sender)
+    }
+    
+    private func initializeUI() {
+        self.navigationController!.navigationBar.shadowImage = UIImage()
+        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+        self.navigationController!.navigationBar.translucent = false
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.flatSandColorDark()
+        self.navigationController?.navigationBar.tintColor = UIColor.flatCoffeeColorDark()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.flatCoffeeColorDark()]
+        
+        changeButtonImageTintColor(previousDayButton, color: UIColor.flatCoffeeColorDark())
+        changeButtonImageTintColor(nextDayButton, color: UIColor.flatCoffeeColorDark())
+        
+        self.dateLabel.textColor = UIColor.flatCoffeeColorDark()
+        self.headerView.backgroundColor = UIColor.flatSandColorDark()
+        self.menuTable.backgroundColor = UIColor.flatCoffeeColor()
+    }
+    
+    private func initializeControls() {
+        changeMenuImage(lunchButton)
+        changeButtonFocus(lunchButton)
+    }
+    
+    private func changeButtonImageTintColor(button: UIButton, color: UIColor) {
+        let originalImage = button.imageView?.image
+        let tintedImage = originalImage!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        button.setImage(tintedImage, forState: .Normal)
+        button.tintColor = color
+    }
+    
+    private func changeMenuImage(button: UIButton) {
+        switch button {
+        case lunchButton:
+            self.menuImage.image = UIImage(named: "Lunch")
+            break
+        case dinnerButton:
+            self.menuImage.image = UIImage(named: "Dinner")
+            break
+        default:
+            break
+        }
+    }
+    
+    private func changeButtonFocus(button: UIButton) {
+        if button.backgroundColor == UIColor.flatCoffeeColor() {
+            return
+        }
+        
+        self.lunchButton.backgroundColor = UIColor.flatCoffeeColorDark()
+        self.dinnerButton.backgroundColor = UIColor.flatCoffeeColorDark()
+        button.backgroundColor = UIColor.flatCoffeeColor()
     }
     
     private func createExampleMenu() {
